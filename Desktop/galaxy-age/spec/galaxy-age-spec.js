e1368cc0b43def91b767d2b41c37526f3a46d8dd
Nick Ramsey <nramseysc@gmail.com>
2019-06-28 16:54:49 -0700

    final test specs 11/11

--- Desktop/galaxy-age/spec/galaxy-age-spec.js
+++ Desktop/galaxy-age/spec/galaxy-age-spec.js
@@ -1,12 +1,50 @@
-import {earthAge, ageDays} from './../src/age-calc.js';
+import {earthAge, ageDays, mercuryAge, venusAge, marsAge, jupiterAge, yearsLeftEarth, yearsLeftMercury, yearsLeftVenus, yearsLeftMars, yearsLeftJupiter} from './../src/age-calc.js';
 describe ('galctic age', function() {
 
   it('should calculate an age in (Earth) years based off a given birthdate', function() {
     expect(earthAge(new Date("1994/2/14"), new Date())).toEqual(25);
-    console.log(earthAge(new Date("1994/2/14"), new Date()));
+    // console.log(earthAge(new Date("1994/2/14"), new Date()));
   });
 
   it('should convert age in (Earth) years to days', function(){
     expect(ageDays(25)).toEqual(9125);
+  });
+
+  it('should calculate age in Mercury years', function(){
+    expect(mercuryAge(9125)).toEqual(104);
+  });
+
+  it('should calculate age in Venus years', function(){
+    expect(venusAge(9215)).toEqual(41);
+  });
+
+  it('should calculate age in Mars years', function(){
+    expect(marsAge(9215)).toEqual(13);
+  });
+
+  it('should calculate age in Jupiter years', function(){
+    expect(jupiterAge(9215)).toEqual(2);
+  });
+
+  it('should calculate how many years left on earth', function(){
+    expect(yearsLeftEarth(25)).toEqual(54);
+  });
+
+  it('should calculate how many years left on Mercury', function(){
+    expect(yearsLeftMercury(104)).toEqual(224);
+  });
+
+  it('should calculate how many years left on Venus', function(){
+    expect(yearsLeftVenus(41)).toEqual(87);
+  });
+
+  it('should calculate how many years left on Mars', function(){
+    expect(yearsLeftMars(13)).toEqual(29);
+  });
+
+  it('should calculate how many years left on Jupiter', function(){
+    expect(yearsLeftJupiter(2)).toEqual(5);
   })
+//   it('should calculate age in Venus years', function(){
+//     expect(venusAge(9125)).toEqual(40);
 });
--- Desktop/galaxy-age/src/age-calc.js
+++ Desktop/galaxy-age/src/age-calc.js
@@ -5,3 +5,39 @@ export function earthAge (birthdate, now) {
 export function ageDays (earthAge) {
   return earthAge*365;
 }
+
+export function mercuryAge (ageDays) {
+  return Math.round(ageDays/88);
+}
+
+export function venusAge (ageDays) {
+  return Math.round(ageDays/226);
+}
+
+export function marsAge (ageDays) {
+  return Math.round(ageDays/686);
+}
+
+export function jupiterAge (ageDays) {
+  return Math.round(ageDays/4329);
+}
+
+export function yearsLeftEarth(earthAge) {
+  return 79-earthAge;
+}
+
+export function yearsLeftMercury(mercuryAge) {
+  return 328-mercuryAge;
+}
+
+export function yearsLeftVenus(venusAge) {
+  return 128-venusAge;
+}
+
+export function yearsLeftMars(marsAge) {
+  return 42-marsAge;
+}
+
+export function yearsLeftJupiter(jupiterAge) {
+  return 7-jupiterAge;
+}
